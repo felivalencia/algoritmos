f8ca1b4e03804c6785df0fa030782bac605fbd56
felivalencia <nbyfceryhxet@hotmail.com>
2024-03-16 18:24:50 -0300

    added the version, left an algorithm, and shared all my lol passwords with you

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # algoritmos
 algoritmos js
+
+
+## como le va doctor? aca le voy a dejar un algoritmo que quiero trabajar pero no termino de entender la consigna, queria ver si me podia ayudar a entenderla, ya que siento que lo entiendo pero quiero aprovechar la oportunidad tambien para hacer esto! , lo deje en el archivo de js, ojala este teniendo un buen fin de semana 
---
+++ algos.js
@@ -0,0 +1,10 @@
+/*Declare a function 'nextLexGreater' that takes a string as input and returns a modified version of the string. The function should rearrange the characters in the string in such a way that it produces the next lexicographically greater permutation of the characters. If such a permutation is not possible, the function should return the original string.
+*/
+
+function nextLexGreater(tring) {
+  
+}
+
+console.log(nextLexGreater('oooooo'));	    // 'oooooo'
+console.log(nextLexGreater('QRSTUVUTSRQ'));	// 'QRSTVQRSTUU'
+console.log(nextLexGreater('yzxwvut'));	    // 'ztuvwxy'
